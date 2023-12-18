0a3095511cb794b51aba8dd64a81fd40616d4f8d
Talha Ansari <talhaansari1606@gmail.com>
2023-12-18 17:01:05 +0530

    changed profile image

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -59,40 +59,40 @@ const Header = () => {
           />
           <img className="h-full md:hidden" src={ytLogoMobile} alt="Youtube" />
         </Link>
-
       </div>
-        <div className="group flex items-center">
-          <div className="flex h-8 md:h-10 md:ml-10 md:pl-5 border border-[#303030] rounded-l-3xl group-focus-within:border-blue-500 md:group-focus-within:ml-5 md:group-focus-within:pl-0  ">
-            <div className="w-10 items-center justify-center hidden group-focus-within:md:flex">
-              <IoIosSearch className="text-white text-xl" />
-            </div>
-            <input
-              type="text"
-              className="bg-transparent outline-none text-white pr-5 pl-5 md:pl-0 w-44 md:group-focus-within:pl-0 md:w-64 lg:w-[500px]"
-              onChange={(e) => setSearchQuery(e.target.value)}
-              onKeyUp={searchQueryHandler}
-              value={searchQuery}
-            />
-          </div>
-          <button
-            onClick={(e) => searchQueryHandler(e, "searchButton")}
-           className="w-[40px] md:w-[60px] h-8 md:h-10 flex items-center justify-center border border-l-0 border-[#303030] rounded-r-3xl bg-white/[0.1] ">
+      <div className="group flex items-center">
+        <div className="flex h-8 md:h-10 md:ml-10 md:pl-5 border border-[#303030] rounded-l-3xl group-focus-within:border-blue-500 md:group-focus-within:ml-5 md:group-focus-within:pl-0  ">
+          <div className="w-10 items-center justify-center hidden group-focus-within:md:flex">
             <IoIosSearch className="text-white text-xl" />
-          </button>
-        </div>
-        <div className="flex justify-center items-center">
-          <div className="hidden md:flex">
-            <div className="flex items-center justify-center h-10 w-10 rounded-full hover:bg-[#303030]/[0.6]">
-              <RiVideoAddLine className="text-white text-xl cursor-pointer"/>
-            </div>
-            <div className="flex ml-2 items-center justify-center h-10 w-10 rounded-full hover:bg-[#303030]/[0.6]">
-              <FiBell className="text-white text-xl cursor-pointer"/>
-            </div>
           </div>
-            <div className="flex h-8 w-8 overflow-hidden md:ml-4 rounded-full justify-center items-center">
-              <img src="https://xsgames.co/randomusers/assets/avatars/female/22.jpg" />
-            </div>
+          <input
+            type="text"
+            className="bg-transparent outline-none text-white pr-5 pl-5 md:pl-0 w-44 md:group-focus-within:pl-0 md:w-64 lg:w-[500px]"
+            onChange={(e) => setSearchQuery(e.target.value)}
+            onKeyUp={searchQueryHandler}
+            value={searchQuery}
+          />
         </div>
+        <button
+          onClick={(e) => searchQueryHandler(e, "searchButton")}
+          className="w-[40px] md:w-[60px] h-8 md:h-10 flex items-center justify-center border border-l-0 border-[#303030] rounded-r-3xl bg-white/[0.1] "
+        >
+          <IoIosSearch className="text-white text-xl" />
+        </button>
+      </div>
+      <div className="flex justify-center items-center">
+        <div className="hidden md:flex">
+          <div className="flex items-center justify-center h-10 w-10 rounded-full hover:bg-[#303030]/[0.6]">
+            <RiVideoAddLine className="text-white text-xl cursor-pointer" />
+          </div>
+          <div className="flex ml-2 items-center justify-center h-10 w-10 rounded-full hover:bg-[#303030]/[0.6]">
+            <FiBell className="text-white text-xl cursor-pointer" />
+          </div>
+        </div>
+        <div className="flex h-8 w-8 overflow-hidden md:ml-4 rounded-full justify-center items-center">
+          <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRDwmG52pVI5JZfn04j9gdtsd8pAGbqjjLswg&usqp=CAU" />
+        </div>
+      </div>
     </div>
   );
 };
